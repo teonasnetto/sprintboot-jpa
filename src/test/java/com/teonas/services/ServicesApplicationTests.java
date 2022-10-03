d6cb2064f90c041b3f5ca52e28edc345c68ac8d4
teonasnetto <teonasnetto@gmail.com>
2022-10-03 15:22:21 -0300

    Project created

---
+++ src/test/java/com/teonas/services/ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teonas.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
