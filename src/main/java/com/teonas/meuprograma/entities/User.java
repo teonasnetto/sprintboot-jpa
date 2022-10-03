1a75a5995ef8bd5cb6bc81fcb376dbabb76a2428
teonasnetto <teonasnetto@gmail.com>
2022-10-03 20:20:19 -0300

    Oder, Instant, ISO 8601

--- src/main/java/com/teonas/meuprograma/entities/User.java
+++ src/main/java/com/teonas/meuprograma/entities/User.java
@@ -1,11 +1,14 @@
 package com.teonas.meuprograma.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +22,13 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     public User() {
     }
