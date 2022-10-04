f2e67792b1669806072f7523bbac6f201420df18
teonasnetto <teonasnetto@gmail.com>
2022-10-03 21:51:38 -0300

    OrderItem, manyToMany association with extra attributes

--- src/main/java/com/teonas/meuprograma/config/TestConfig.java
+++ src/main/java/com/teonas/meuprograma/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.teonas.meuprograma.entities.Category;
 import com.teonas.meuprograma.entities.Order;
+import com.teonas.meuprograma.entities.OrderItem;
 import com.teonas.meuprograma.entities.Product;
 import com.teonas.meuprograma.entities.User;
 import com.teonas.meuprograma.entities.enums.OrderStatus;
 import com.teonas.meuprograma.repositories.CategoryRepository;
+import com.teonas.meuprograma.repositories.OrderItemRepository;
 import com.teonas.meuprograma.repositories.OrderRepository;
 import com.teonas.meuprograma.repositories.ProductRepository;
 import com.teonas.meuprograma.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -67,6 +72,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 
 }
--- src/main/java/com/teonas/meuprograma/entities/Order.java
+++ src/main/java/com/teonas/meuprograma/entities/Order.java
@@ -2,6 +2,8 @@ package com.teonas.meuprograma.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.teonas.meuprograma.entities.enums.OrderStatus;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,6 +33,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -63,6 +69,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
