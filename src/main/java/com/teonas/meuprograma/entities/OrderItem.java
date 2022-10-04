2c50b81f111e77dd129c99c4d57ed7d2bab8b7b2
teonasnetto <teonasnetto@gmail.com>
2022-10-04 16:50:58 -0300

    Subtotal e Total methods

--- src/main/java/com/teonas/meuprograma/entities/OrderItem.java
+++ src/main/java/com/teonas/meuprograma/entities/OrderItem.java
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable{
         this.price = price;
     }
 
+    public Double getSubTotal() {
+        return price * Quantity;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
--- src/main/java/com/teonas/meuprograma/entities/Payment.java
+++ src/main/java/com/teonas/meuprograma/entities/Payment.java
@@ -3,6 +3,8 @@ package com.teonas.meuprograma.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +22,7 @@ public class Payment implements Serializable{
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
