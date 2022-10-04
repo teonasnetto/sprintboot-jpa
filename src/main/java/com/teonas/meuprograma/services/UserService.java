927da022428c621498188f820a090b0f31833b8c
teonasnetto <teonasnetto@gmail.com>
2022-10-04 17:47:12 -0300

    Exception handling - update

--- src/main/java/com/teonas/meuprograma/services/UserService.java
+++ src/main/java/com/teonas/meuprograma/services/UserService.java
@@ -13,9 +13,11 @@ import com.teonas.meuprograma.repositories.UserRepository;
 import com.teonas.meuprograma.services.exceptions.DatabaseException;
 import com.teonas.meuprograma.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repository;
 
@@ -34,7 +36,7 @@ public class UserService {
 
     public void delete(Long id) {
         try {
-        repository.deleteById(id);
+            repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
         } catch (DataIntegrityViolationException e) {
@@ -43,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
