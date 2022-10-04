96805fc8eb46ff5b4212c682cc43f818fe0bed67
teonasnetto <teonasnetto@gmail.com>
2022-10-04 16:11:44 -0300

    Payment oneToOne association

--- src/main/java/com/teonas/meuprograma/config/TestConfig.java
+++ src/main/java/com/teonas/meuprograma/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.teonas.meuprograma.entities.Category;
 import com.teonas.meuprograma.entities.Order;
 import com.teonas.meuprograma.entities.OrderItem;
+import com.teonas.meuprograma.entities.Payment;
 import com.teonas.meuprograma.entities.Product;
 import com.teonas.meuprograma.entities.User;
 import com.teonas.meuprograma.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
