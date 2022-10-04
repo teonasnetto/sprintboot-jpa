475f49f7893796ddf37b01d70d4491803de5ec36
teonasnetto <teonasnetto@gmail.com>
2022-10-03 21:01:45 -0300

    Category

---
+++ src/main/java/com/teonas/meuprograma/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.teonas.meuprograma.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teonas.meuprograma.entities.Category;
+import com.teonas.meuprograma.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
