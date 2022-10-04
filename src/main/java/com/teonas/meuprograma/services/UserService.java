fc5f1bd6e66c94dcc96775f364a3ad3fd6a51ad5
teonasnetto <teonasnetto@gmail.com>
2022-10-04 17:36:48 -0300

    Exception handling - findById

--- src/main/java/com/teonas/meuprograma/services/UserService.java
+++ src/main/java/com/teonas/meuprograma/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.teonas.meuprograma.entities.User;
 import com.teonas.meuprograma.repositories.UserRepository;
+import com.teonas.meuprograma.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/teonas/meuprograma/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.teonas.meuprograma.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+    
+}
